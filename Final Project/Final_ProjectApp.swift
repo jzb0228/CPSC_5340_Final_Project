3e1c3bb0063e8bc3a26a94cef45de507bcd1cafc
Justin Baik <jzb0228@auburn.edu>
2024-06-30 17:34:26 -0700

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Justin Baik on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
